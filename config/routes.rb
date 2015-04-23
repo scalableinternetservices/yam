82f1c7f01ba2e743982ec8f670b97803b2a866ea
Crystal Hsieh <crystalhsieh7@gmail.com>
2015-04-23 12:16:32 -0700

    made static home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :games
 
-  root 'home#index', as: 'home'
+  root 'static_pages#home', as: 'home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
