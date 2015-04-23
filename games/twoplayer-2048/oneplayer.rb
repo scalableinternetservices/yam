d9e842337421afdbf4e4ca6fd417b278d77d439b
Ryan Voong <ryanvoong@ucla.edu>
2015-04-23 11:34:31 -0700

    Fixed board initialization. Gameplay is still buggy though.

--- games/twoplayer-2048/oneplayer.rb
+++ games/twoplayer-2048/oneplayer.rb
@@ -1,18 +1,18 @@
 #!/bin/ruby 
 
-require 'matrix' 
+require 'matrix'
+require 'io/console'
 
 # The board, in charge of tracking its own state
-class Board 
+class Board
     def initialize()
-        # Boards hold 'false' for an empty cell 
+        # Boards hold 'false' for an empty cell
         # and a number otherwise
-        @board = Matrix(4, 4)
-        @board.each { |elem| elem = false}
+        @board = Matrix.build(4, 4) { false }
     end
 
     def apply_move(move) 
-        nboard = Matrix(4, 4)
+        nboard = Matrix.build(4, 4)
         case move
             when :up
                 nboard = Matrix[ 
@@ -73,7 +73,7 @@ class Board
         }
         ).reverse
 
-        while nrow.length < row.length
+        while nrow.size < row.size
             nrow << false
         end
 
@@ -84,7 +84,7 @@ class Board
 
     # Value is what user wants to plop in at (row, column)
     def place_piece(row, column, value)
-        nboard = Matrix.build(4) { |r, c| 
+        nboard = Matrix.build(4) { |r, c|
             # Matching coordinate and nothing already there
             if r == row && c == column && !@board[r,c]
                value
@@ -100,14 +100,17 @@ class Board
 
     def print
         @board.row_size.times do |i|
-            puts @board.row(i).join(",")
+            puts @board.row(i).to_a.join(",")
         end
+        puts ""
     end
 end
 
 class Game
     def initialize()
         @board = Board.new
+        #@board.place_piece(0, 0, 1)
+        @board.place_piece(1, 1, 1)
     end
 
 public
@@ -116,13 +119,13 @@ public
     def play()
         loop do
             @board.print
-            #read_char
-
+            @board.apply_move(key2sym(read_char))
         end
     end
 
     # Internal helper functions
-    private
+private
+
     # Read in keyboard input
     # Original from https://gist.github.com/acook/4190379
     def read_char
@@ -166,10 +169,13 @@ end
 #puts "[ #{collapse_row( [1, false, 1] ).join(",")} ]"
 #puts "[ #{collapse_row( [1, 1, 2, 3] ).join(",")} ]"
 
-test_board = Matrix[
-    [ false, false, false, false ], 
-    [ false, 2,     false, false ], 
-    [ false, false, false, false ],
-    [ false, false, false, false ]
-]
-puts place_piece(test_board, 0, 0, 9)
+# test_board = Matrix[
+#     [ false, false, false, false ],
+#     [ false, 2,     false, false ],
+#     [ false, false, false, false ],
+#     [ false, false, false, false ]
+# ]
+# puts place_piece(test_board, 0, 0, 9)
+
+new_game = Game.new
+new_game.play
