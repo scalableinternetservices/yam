cc5bf7d6e78a39ec98a4002a3fd906cf2b6fc23a
KyCodeHuynh <KyCodeHuynh@gmail.com>
2015-05-02 12:02:00 -0700

    Remove or comment out default unit tests to replace with our own.

--- test/controllers/game2048_controller_test.rb
+++ test/controllers/game2048_controller_test.rb
@@ -1,14 +1,9 @@
 require 'test_helper'
 
 class Game2048ControllerTest < ActionController::TestCase
-  test "should get move" do
-    get :move
-    assert_response :success
-  end
-
-  test "should get place" do
-    get :place
-    assert_response :success
-  end
-
+  # Save as example
+  # test "should get move" do
+  #   get :move
+  #   assert_response :success
+  # end
 end
