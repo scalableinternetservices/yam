84f8df205f8f7cb4e3d1aad6dfd16c2f37b520e6
Ryan Voong <ryanvoong@ucla.edu>
2015-05-21 12:43:54 -0700

    Fixed user creation in seeds.rb. $ rake db:seed is now working!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-0.upto(1000) do |n|
-  User.create(email: "test#{n}@test.com", encrypted_password: "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3") 
-  # Game2048.create(pid1: n, pid2: n + 1000,
-  #               board1: Board.new.to_s, board2: Board.new.to_s, player1turn: true)
+# create 100 users
+100.times do |n|
+  User.create!(id: n, email: "test#{n}@test.com", password: 'password', password_confirmation: 'password')
+  puts "Created user: test#{n}@test.com"
 end
+puts "Number of users: #{User.count}"
