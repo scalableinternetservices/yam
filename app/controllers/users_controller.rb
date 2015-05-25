b7b2245513fa840dc63452036ae7b779f5592d74
Ryan Voong <ryanvoong@ucla.edu>
2015-05-24 21:47:48 -0700

    Added client-side caching for user profiles.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 	# Controller to handle user profiles
 	def show
-		@user = User.find(params[:id])
+		@user = User.find(params[:id]) if stale?(current_user)
 	end
-
 end
