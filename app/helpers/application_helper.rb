b7fdf9a13e9fae701dd4e93c320c7d429f1643de
Crystal Hsieh <crystalhsieh7@gmail.com>
2015-05-29 16:01:30 -0700

    fixed caching stuff lol

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,9 @@
 module ApplicationHelper
-  def cache_key_for_navigation
-	"navigation-bar"
-  end
-
   def cache_key_for_javascript
 	"navigation-bar-javascript"
   end
 
-
-
+  def cache_key_for_nav
+	"navigation-bar"
+  end
 end
--- app/helpers/game2048_helper.rb
+++ app/helpers/game2048_helper.rb
@@ -1,5 +1,2 @@
 module Game2048Helper
-  def cache_key_for_waitroom
-	"cache_waitroom"
-  end    
 end
