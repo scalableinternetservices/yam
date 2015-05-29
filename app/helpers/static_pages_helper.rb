1e3a91252927a4dc80d3c388cdf8a45940778935
Crystal Hsieh <crystalhsieh7@gmail.com>
2015-05-29 10:58:27 -0700

    fixing cache keys.. time can't work as a parameter or the key will always change

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,8 +1,12 @@
 module StaticPagesHelper
+=begin
   def cache_key_for_instructions
 	"instructions-#{Time.now.to_i}"
   end
-
+  def cache_key_for_about
+	"about-#{Time.now.to_i}"
+  end
+=end
 end
 
 
