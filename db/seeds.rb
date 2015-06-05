8dfbc162ac44f6930ea82c2b8d09b3142137c628
Ryan Voong <ryanvoong@gmail.com>
2015-06-05 10:59:06 -0700

    Seed 48 users for the demo and don't seed any games.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Create 10000 users and games
-100000.times do |n|
+# Create 48 users and games
+48.times do |n|
   User.create!(id: n, email: "test#{n}@test.com", password: 'password', password_confirmation: 'password', gamer_tag: "user#{n}")
   # puts "Created user: test#{n}@test.com"
 
-  Game2048.create(pid1: n, pid2: n + 1,
-                    board1: "1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0", 
-                    board2: "1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0", 
-                    player1turn: true)
+  # Game2048.create(pid1: n, pid2: n + 1,
+  #                   board1: "1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
+  #                   board2: "1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
+  #                   player1turn: true)
   # puts "Created a Game2048 match"
 end
 puts "Number of users: #{User.count}"
